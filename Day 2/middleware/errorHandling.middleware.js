02539680a46e17d5437accd78117d13071a5d2a5
Seif <seifahmedjr7@gmail.com>
2026-02-12 20:50:57 +0200

    Remove unused files and routes from Day 2 project
    - Deleted package.json to remove dependencies and scripts.
    - Removed admin.route.js and teacher.route.js for unused routes.
    - Eliminated server.js as the entry point for the application.
    - Deleted empty admin.service.js and teacher.service.js files.
    - Removed path.js utility file as it is no longer needed.

--- Day 2/middleware/errorHandling.middleware.js
+++
@@ -1,7 +0,0 @@
-export default (err, req, res, next)=>{
-    console.log(err);
-    const errorCode = err.status || 500
-    const message = err.message || "Internal Server Error"
-    return res
-    .status(errorCode).json({message})
-}
--- Day 2/middleware/notFound.middleware.js
+++
@@ -1,5 +0,0 @@
-export default (req, res) => {
-  res
-    .status(400)
-    .json({ message: "Bad Request, make sure u are in the right url" });
-};
