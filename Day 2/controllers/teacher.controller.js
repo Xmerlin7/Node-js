3e42e97340f721cb6bed913e1f549362ea584b17
Seif <seifahmedjr7@gmail.com>
2026-02-12 18:42:56 +0200

    feat: implement teacher controller with CRUD function placeholders

---
+++ Day 2/controllers/teacher.controller.js
@@ -0,0 +1,16 @@
+
+const getAll = (req, res) => {
+    res.status(200).json({message: "welcome"})
+};
+const getByID = () => {};
+const addOne = () => {};
+const deleteOne = () => {};
+const updateOne = () => {};
+
+export {
+  getAll,
+  getByID,
+  updateOne,
+  addOne,
+  deleteOne,
+};
