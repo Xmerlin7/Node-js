92a2fa84c01591badba5e813f4cab3910184818a
Seif <seifahmedjr7@gmail.com>
2026-02-12 19:11:34 +0200

    fix(controller): update getAll function to include error handling

--- Day 2/controllers/teacher.controller.js
+++ Day 2/controllers/teacher.controller.js
@@ -1,16 +1,14 @@
-
-const getAll = (req, res) => {
-    res.status(200).json({message: "welcome"})
+const getAll = (req, res, next) => {
+  try {
+    throw new Error("تجربة: خطأ متعمد في جلب المدرسين!");
+  } catch (error) {
+    next(error);
+  }
 };
+
 const getByID = () => {};
 const addOne = () => {};
 const deleteOne = () => {};
 const updateOne = () => {};
 
-export {
-  getAll,
-  getByID,
-  updateOne,
-  addOne,
-  deleteOne,
-};
+export { getAll, getByID, updateOne, addOne, deleteOne };
