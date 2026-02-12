59d20474eb9cdbbac81da1abcf2274868ec19164
Seif <seifahmedjr7@gmail.com>
2026-02-13 00:00:30 +0200

    fix(teacher.controller): update getAll function to call getAllTeachers and implement missing controller methods

--- Day 2/controllers/teacher.controller.js
+++ Day 2/controllers/teacher.controller.js
@@ -1,16 +1,41 @@
-import * as teacherService from "../services/teacher.service.js"
+import * as teacherService from "../services/teacher.service.js";
 const getAll = async (req, res, next) => {
   try {
-    let teachers = await teacherService.getAll();
+    let teachers = await teacherService.getAllTeachers();
     res.status(200).json(teachers);
   } catch (error) {
     next(error);
   }
 };
 
-const getByID = () => {};
-const addOne = () => {};
-const deleteOne = () => {};
-const updateOne = () => {};
+const getByID = async (req, res, next) => {
+  try {
+    let teacher = await teacherService.getTeacherByID(req.params.id);
+    return res.status(200).json(teacher);
+  } catch (error) {
+    next(error);
+  }
+};
+const addOne = async (req, res, next) => {
+  try {
+    const createdTeacher = await teacherService.addTeacher(req.body);
+    return res.status(201).json(createdTeacher);
+  } catch (error) {
+    next(error);
+  }
+};
+const deleteOne = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+const updateOne = async (req, res, next) => {
+  try {
+    await teacherService.addTeacher(req.body);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export { getAll, getByID, updateOne, addOne, deleteOne };
