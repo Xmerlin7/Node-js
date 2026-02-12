91d3b9e659ae3c56840bed5bbbc0c81d897cfeb5
Seif <seifahmedjr7@gmail.com>
2026-02-12 21:38:25 +0200

    fix(path.js): correct export statement for root variable

--- Day 2/util/path.js
+++ Day 2/util/path.js
@@ -3,4 +3,5 @@ import path from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-export default root = path.resolve(__dirname, "..");
+const root = path.resolve(__dirname, "..");
+export default root
