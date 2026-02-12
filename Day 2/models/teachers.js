3959d6127edfcfc9e775f6d8c8d8af3dd016e03a
Seif <seifahmedjr7@gmail.com>
2026-02-12 21:38:15 +0200

    feat(teachers.js): implement Teacher class with constructor and getAll method

--- Day 2/models/teachers.js
+++ Day 2/models/teachers.js
@@ -1,2 +1,18 @@
-import fs from "fs/promises"
-import Root_Dir from "../util/path.js"
+import fs from "fs/promises";
+import path from "path";
+import Root_Dir from "../util/path.js";
+
+export default class Teacher {
+  constructor(name, email, subject, experience) {
+    this.id;
+    this.name = name;
+    this.email = email;
+    this.subject = subject;
+    this.experience = experience;
+  }
+
+  static async getAll() {
+    let data = await fs.readFile(path.join(Root_Dir, "data", "teachers.json"), "utf-8")
+    return JSON.parse(data);
+  }
+}
