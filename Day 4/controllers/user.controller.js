443e6c37bd3a179d1dbbec9ae3aee4cf779fa688
Seif <seifahmedjr7@gmail.com>
2026-02-20 01:46:53 +0200

    feat(controller): add user controller with create and get all users functions

---
+++ Day 4/controllers/user.controller.js
@@ -0,0 +1,20 @@
+import * as userService from "../services/user.service.js";
+
+export const createOne = async (req, res, next) => {
+  try {
+    let user = await userService.create(req.body);
+    res.status(200).json({ message: "User Created successfully!", data: user });
+  } catch (error) {
+    next(error);
+  }
+};
+export const getAllUsers = async (req, res, next) => {
+  try {
+    let users = await userService.getAll(req.body);
+    res
+      .status(200)
+      .json({ message: "User retrieved successfully!", data: users });
+  } catch (error) {
+    next(error);
+  }
+};
