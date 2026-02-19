807698de1640f4e2d13a7f4a26ed9fbdf164a106
Seif <seifahmedjr7@gmail.com>
2026-02-20 01:46:35 +0200

    feat(routes): add user routes for user-related API endpoints

--- Day 4/app.js
+++ Day 4/app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import categoryRouter from "./routes/category.route.js";
 import productRouter from "./routes/product.route.js";
+import userRouter from "./routes/user.route.js";
 import globalErrorHandler from "./middlewares/errors/errorHandler.js";
 const app = express();
 app.use(morgan("dev"));
@@ -13,7 +14,7 @@ app.use(cookieParser());
 
 app.use("/api/category", categoryRouter);
 app.use("/api/product", productRouter);
-
+app.use("/api/user", userRouter);
 //Centralized global Error handling
 app.use(globalErrorHandler);
 export default app;
