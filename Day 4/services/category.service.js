ba87e8ff5121c0b6878581947a5c3bedc0800e99
Seif <seifahmedjr7@gmail.com>
2026-02-19 23:59:31 +0200

    fix(service): update parameter name in create function for clarity

--- Day 4/services/category.service.js
+++ Day 4/services/category.service.js
@@ -1,4 +1,4 @@
 import Category from "../models/category.js";
-export const create = async (body) => {
-    await Category.create({name: body.name})
+export const create = async (cat) => {
+    await Category.create({name: cat.name})
 };
