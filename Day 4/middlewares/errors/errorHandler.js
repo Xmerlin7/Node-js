d312c2a23e721d1170e0ffd601cd59d51ee1a5c0
Seif <seifahmedjr7@gmail.com>
2026-02-19 21:32:50 +0200

    feat(middlewares): enhance error handler for validation and duplicate key errors

--- Day 4/middlewares/errors/errorHandler.js
+++ Day 4/middlewares/errors/errorHandler.js
@@ -1,5 +1,21 @@
-export default (error, req, res, next) => {
-  let status = error.status || 500;
-  let mess = error.message || "Internal Server Error!";
-  return res.status(status).json(mess);
+export default (err, req, res, next) => {
+  if (err.name === "ValidationError") {
+    let errMsgs = Object.values(err.errors)
+      .map((el) => el.message)
+      .join(", ");
+    return res.status(400).json({
+      message: errMsgs,
+    });
+  }
+  if (err.code === 11000) {
+    console.log(err.keyValue);
+    const message = Object.entries(err.keyValue).map(
+      ([key, value]) => `${key} ${value} already exists`,
+    );
+    return res.status(400).json(message);
+  }
+
+  res.status(err.status || 500).json({
+    message: err.message || "Internal Server Error!",
+  });
 };
