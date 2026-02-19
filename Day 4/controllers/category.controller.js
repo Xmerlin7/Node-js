3a4e2279c73e2fbadebbc0cb23f29647427dd42f
Seif <seifahmedjr7@gmail.com>
2026-02-19 23:58:59 +0200

    feat(controller): update createCategory response to include created data

--- Day 4/controllers/category.controller.js
+++ Day 4/controllers/category.controller.js
@@ -3,7 +3,9 @@ import * as categoryServices from "../services/category.service.js";
 export const createCategory = async (req, res, next) => {
   try {
     await categoryServices.create(req.body);
-    res.status(200).json({ message: "Category Created Successfully!" });
+    res
+      .status(201)
+      .json({ message: "Category Created Successfully!", data: req.body });
   } catch (err) {
     next(err);
   }
