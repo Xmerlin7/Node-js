5ef0796fb48392fdfd21675b8fe1704d77b8e0a0
Seif <seifahmedjr7@gmail.com>
2026-02-19 14:17:56 +0200

    feat(app): set up express middleware and routing for category API

--- Day 4/app.js
+++ Day 4/app.js
@@ -1,4 +1,17 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import categoryRouter from "./routes/category.route.js";
+import globalErrorHandler from "./middlewares/errors/errorHandler.js";
 const app = express();
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
 
+app.use("/api/category", categoryRouter);
+
+//Centralized global Error handling
+app.use(globalErrorHandler);
 export default app;
