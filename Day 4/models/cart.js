836be5f8984e645d26c76ba09f40f46a3598defa
Seif <seifahmedjr7@gmail.com>
2026-02-20 01:46:59 +0200

    feat(models): add cart model with user and product schema

---
+++ Day 4/models/cart.js
@@ -0,0 +1,30 @@
+import { Schema, model } from "mongoose";
+import product from "./product";
+
+const cartSchema = new Schema(
+  {
+    user: {
+      ref: "User",
+      type: Schema.Types.ObjectId,
+      required: [true, "User is required"],
+    },
+    products: [
+      {
+        product: {
+          type: Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+          default: 1,
+        },
+      },
+    ],
+  },
+  { timestamps: true },
+);
+
+export default model("Cart", cartSchema);
