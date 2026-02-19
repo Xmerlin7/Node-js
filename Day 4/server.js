a3f3ec6804b330a177025b970fc8a7e1a8dc226a
Seif <seifahmedjr7@gmail.com>
2026-02-19 23:58:51 +0200

    feat(server): add logging for server start with listening port

--- Day 4/server.js
+++ Day 4/server.js
@@ -3,4 +3,6 @@ import server from "./app.js";
 import { connectDB } from "./config/database.js";
 dotenv.config();
 connectDB();
-server.listen(process.env.PORT);
+server.listen(process.env.PORT, () => {
+	console.log(`Server listening on port ${process.env.PORT}`);
+});
