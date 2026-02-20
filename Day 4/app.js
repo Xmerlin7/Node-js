868c58ed2bd3337974274bba908c7780ff09fe74
Seif <seifahmedjr7@gmail.com>
2026-02-20 13:45:14 +0200

    feat(routes): add cart routes and fix user/product API endpoint paths

--- Day 4/app.js
+++ Day 4/app.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import categoryRouter from "./routes/category.route.js";
 import productRouter from "./routes/product.route.js";
 import userRouter from "./routes/user.route.js";
+import cartRouter from "./routes/cart.route.js";
 import globalErrorHandler from "./middlewares/errors/errorHandler.js";
 const app = express();
 app.use(morgan("dev"));
@@ -13,8 +14,9 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/category", categoryRouter);
-app.use("/api/product", productRouter);
-app.use("/api/user", userRouter);
+app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
+app.use("/api/carts", cartRouter);
 //Centralized global Error handling
 app.use(globalErrorHandler);
 export default app;
