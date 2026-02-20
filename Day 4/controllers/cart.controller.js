70a5e05ed8c1ac041c2c75930221ae1a63c6c2c6
Seif <seifahmedjr7@gmail.com>
2026-02-20 13:45:21 +0200

    feat(controller): implement cart controller with add, get, and delete functions

---
+++ Day 4/controllers/cart.controller.js
@@ -0,0 +1,20 @@
+import * as cartServices from "../services/cart.service.js";
+
+export const addToCart = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
+export const getUserCart = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
+export const deleteFromCart = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
