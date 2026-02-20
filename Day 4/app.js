fec85c6f58d7e2c19f6ba059554596a5b40baa84
Seif <seifahmedjr7@gmail.com>
2026-02-21 01:53:53 +0200

    feat(routes): add account router to handle user account related routes

--- Day 4/app.js
+++ Day 4/app.js
@@ -6,17 +6,20 @@ import categoryRouter from "./routes/category.route.js";
 import productRouter from "./routes/product.route.js";
 import userRouter from "./routes/user.route.js";
 import cartRouter from "./routes/cart.route.js";
+import accRouter from "./routes/account.route.js";
 import globalErrorHandler from "./middlewares/errors/errorHandler.js";
 import notFound from "./middlewares/errors/notFound.js";
+
 const app = express();
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
+app.use("/api/", accRouter);
+app.use("/api/users", userRouter);
 app.use("/api/category", categoryRouter);
 app.use("/api/products", productRouter);
-app.use("/api/users", userRouter);
 app.use("/api/carts", cartRouter);
 
 // Fall Back Router
