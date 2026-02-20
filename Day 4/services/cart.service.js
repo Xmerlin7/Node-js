1e6a131832340c557175275068edbc9964cf9ab2
Seif <seifahmedjr7@gmail.com>
2026-02-20 13:45:40 +0200

    feat(service): implement cart service with get, add, and delete functions

---
+++ Day 4/services/cart.service.js
@@ -0,0 +1,20 @@
+import cart from "../models/cart.js";
+export const get = async () => {
+  try {
+    
+  } catch (err) {
+    throw err;
+  }
+};
+export const add = async (data) => {
+  try {
+  } catch (err) {
+    throw err;
+  }
+};
+export const del = async (data) => {
+  try {
+  } catch (err) {
+    throw err;
+  }
+};
