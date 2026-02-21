a95c8bd6c76ff41a8f33d4f753eff3c1219203fc
Seif <seifahmedjr7@gmail.com>
2026-02-21 03:54:08 +0200

    feat(routes): add validation for category creation and product retrieval

--- Day 4/routes/category.route.js
+++ Day 4/routes/category.route.js
@@ -5,9 +5,26 @@ import {
 import { Router } from "express";
 import authenticate from "../middlewares/auth/authenticate.middleware.js";
 import authorize from "../middlewares/auth/authorize.middleware.js";
+import validate from "../middlewares/validations/validatorHandler.js";
+import {
+  createCategoryValidator,
+  categoryIdParamValidator,
+} from "../middlewares/validations/category.validators.js";
 
 const router = Router();
 
-router.post("/", authenticate, authorize("admin"), createCategory);
-router.get("/:id/products", getProductsByCategory);
+router.post(
+  "/",
+  authenticate,
+  authorize("admin"),
+  createCategoryValidator,
+  validate,
+  createCategory,
+);
+router.get(
+  "/:id/products",
+  categoryIdParamValidator,
+  validate,
+  getProductsByCategory,
+);
 export default router;
