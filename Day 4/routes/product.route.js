97ec1f502c5d89901cbfbce9a23fd0c9aee05d3d
Seif <seifahmedjr7@gmail.com>
2026-02-21 03:54:15 +0200

    feat(routes): add validation for product creation and retrieval

--- Day 4/routes/product.route.js
+++ Day 4/routes/product.route.js
@@ -6,10 +6,22 @@ import {
 import { Router } from "express";
 import authenticate from "../middlewares/auth/authenticate.middleware.js";
 import authorize from "../middlewares/auth/authorize.middleware.js";
+import validate from "../middlewares/validations/validatorHandler.js";
+import {
+  createProductValidator,
+  productIdParamValidator,
+} from "../middlewares/validations/product.validators.js";
 
 const router = Router();
 
-router.post("/", authenticate, authorize("admin"), createProduct);
+router.post(
+  "/",
+  authenticate,
+  authorize("admin"),
+  createProductValidator,
+  validate,
+  createProduct,
+);
 router.get("/", getProducts);
-router.get("/:id", getProductByID);
+router.get("/:id", productIdParamValidator, validate, getProductByID);
 export default router;
