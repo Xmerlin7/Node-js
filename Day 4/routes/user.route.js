bc0f62cf08a768749554f56243c346447e2d7e74
Seif <seifahmedjr7@gmail.com>
2026-02-21 03:54:21 +0200

    feat(routes): add validation for user creation endpoint

--- Day 4/routes/user.route.js
+++ Day 4/routes/user.route.js
@@ -6,11 +6,19 @@ import {
 } from "../controllers/user.controller.js";
 import authenticate from "../middlewares/auth/authenticate.middleware.js";
 import authorize from "../middlewares/auth/authorize.middleware.js";
+import validate from "../middlewares/validations/validatorHandler.js";
+import { createUserValidator } from "../middlewares/validations/user.validators.js";
 const router = Router();
 
 router
   .route("/")
   .get(authenticate, authorize("admin"), getAllUsers)
-  .post(authenticate, authorize("admin"), createOne);
+  .post(
+    authenticate,
+    authorize("admin"),
+    createUserValidator,
+    validate,
+    createOne,
+  );
 router.get("/id", authenticate, authorize("admin"), getUser);
 export default router;
