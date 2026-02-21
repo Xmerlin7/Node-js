da8d367a6029bff29bf97e94f73764545bba0dc9
Seif <seifahmedjr7@gmail.com>
2026-02-21 03:54:30 +0200

    feat(routes): implement logout and refresh functionality with token validation

--- Day 4/services/account.service.js
+++ Day 4/services/account.service.js
@@ -4,6 +4,16 @@ import User from "../models/user.js";
 import { create } from "./user.service.js";
 import ApiError from "../utils/ApiError.js";
 import RefreshTokenModel from "../models/tokens.js";
+import Tokens from "../models/tokens.js";
+
+export const getCurrentUser = async (req) => {
+  const currentUserId = req.user.userId;
+  const currentUser = await User.findById(currentUserId);
+  console.log(currentUser);
+  return currentUser
+};
+
+
 export const registerUser = async (data) => {
   const newUser = await create(data);
   return newUser;
@@ -21,7 +31,7 @@ export const loginUser = async (data) => {
   const accessToken = await jwt.sign(
     { userId: foundUser._id, role: foundUser.role },
     process.env.ACCESS_TOKEN_SECRET,
-    { expiresIn: "5m" },
+    { expiresIn: "15m" },
   );
   const refreshToken = await jwt.sign(
     {
@@ -42,5 +52,45 @@ export const loginUser = async (data) => {
 
   return { refreshToken, accessToken, foundUserName: foundUser.name };
 };
-export const logoutUser = async (data) => {};
-export const refreshUser = async (data) => {};
+export const logoutUser = async (req) => {
+  const cookieRefreshToken = req.cookies.refreshToken;
+  if (!cookieRefreshToken) throw new ApiError("Refresh Token Required!", 400);
+
+  let payload = jwt.verify(
+    cookieRefreshToken,
+    process.env.REFRESH_TOKEN_SECRET,
+  );
+  const { userId } = payload;
+  const allUserRefTokens = await Tokens.find({ user: userId });
+
+  const comparedBooleanTokens = await Promise.all(
+    allUserRefTokens.map((t) => bcrypt.compare(cookieRefreshToken, t.token)),
+  );
+  const currentTokenIndex = comparedBooleanTokens.findIndex((t) => t == true);
+  if (currentTokenIndex === -1)
+    return new ApiError("Refresh token not found", 401);
+
+  await Tokens.findByIdAndDelete(allUserRefTokens[currentTokenIndex]._id);
+};
+export const refreshUser = async (req) => {
+  const cookieRefreshToken = req.cookies.refreshToken;
+  if (!cookieRefreshToken) throw new ApiError("Refresh Token Required!", 400);
+
+  let payload = jwt.verify(
+    cookieRefreshToken,
+    process.env.REFRESH_TOKEN_SECRET,
+  );
+  const { userId } = payload;
+
+  const newAccessToken = jwt.sign(
+    {
+      userId,
+      role: payload.role,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: "15m",
+    },
+  );
+  return newAccessToken;
+};
