e5c3be275d0aafbf76466f069fdf1877bff51007
Seif <seifahmedjr7@gmail.com>
2026-02-21 03:53:57 +0200

    feat(routes): enhance account routes with validation for login, register, logout, and refresh

--- Day 4/routes/account.route.js
+++ Day 4/routes/account.route.js
@@ -1,9 +1,24 @@
 import { Router } from "express";
-import { login, register, getUser } from "../controllers/account.controller.js";
+import {
+  login,
+  register,
+  getUser,
+  logout,
+  refresh,
+} from "../controllers/account.controller.js";
 import authenticate from "../middlewares/auth/authenticate.middleware.js";
+import validate from "../middlewares/validations/validatorHandler.js";
+import {
+  loginValidator,
+  registerValidator,
+  refreshValidator,
+  logoutValidator,
+} from "../middlewares/validations/account.validators.js";
 const router = Router();
 
-router.route("/login").post(login);
-router.route("/register").post(register);
+router.route("/login").post(loginValidator, validate, login);
+router.route("/register").post(registerValidator, validate, register);
+router.post("/logout", authenticate, logoutValidator, validate, logout);
+router.post("/refresh", refreshValidator, validate, refresh);
 router.get("/me", authenticate, getUser);
 export default router;
