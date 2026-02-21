b3063ad0603b28e5739dec48e726d857919c6180
Seif <seifahmedjr7@gmail.com>
2026-02-21 03:52:44 +0200

    feat(auth): add logout and refresh functionality, and retrieve current user details

--- Day 4/controllers/account.controller.js
+++ Day 4/controllers/account.controller.js
@@ -1,4 +1,10 @@
-import { registerUser, loginUser } from "../services/account.service.js";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshUser,
+  getCurrentUser,
+} from "../services/account.service.js";
 
 export const register = async (req, res, next) => {
   try {
@@ -28,11 +34,39 @@ export const login = async (req, res, next) => {
     next(error);
   }
 };
+export const logout = async (req, res, next) => {
+  try {
+    await logoutUser(req);
+    res
+      .status(200)
+      .clearCookie("refreshToken", {
+        httpOnly: true,
+        sameSite: "strict",
+      })
+      .json({
+        message: `logged Out successfully!`,
+      });
+  } catch (error) {
+    next(error);
+  }
+};
+export const refresh = async (req, res, next) => {
+  try {
+    const accessToken = await refreshUser(req);
+    res.status(200).json({
+      message: `U refreshed successfully!`,
+      data: accessToken,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 export const getUser = async (req, res, next) => {
   try {
+    let { name, role } = await getCurrentUser(req);
     return res
       .status(200)
-      .json({ message: "Retrieved Successfully!", data: req.user });
+      .json({ message: "Retrieved Successfully!", data: [name, role] });
   } catch (error) {
     next(error);
   }
