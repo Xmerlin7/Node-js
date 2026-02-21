3cd6a2a5bd70dfbc7e70e4ee90a230cc7c066e2b
Seif <seifahmedjr7@gmail.com>
2026-02-21 03:54:03 +0200

    feat(routes): add validation for cart operations including add, delete, and get user cart

--- Day 4/routes/cart.route.js
+++ Day 4/routes/cart.route.js
@@ -1,5 +1,11 @@
 import { Router } from "express";
 import authenticate from "../middlewares/auth/authenticate.middleware.js";
+import validate from "../middlewares/validations/validatorHandler.js";
+import {
+  addToCartValidator,
+  cartUserIdParamValidator,
+  deleteFromCartValidator,
+} from "../middlewares/validations/cart.validators.js";
 import {
   addToCart,
   getUserCart,
@@ -9,7 +15,7 @@ const router = new Router();
 router
   .route("/user/:userId")
   .all(authenticate)
-  .post(addToCart)
-  .delete(deleteFromCart)
-  .get(getUserCart);
+  .post(addToCartValidator, validate, addToCart)
+  .delete(deleteFromCartValidator, validate, deleteFromCart)
+  .get(cartUserIdParamValidator, validate, getUserCart);
 export default router;
